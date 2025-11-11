558d537744cad15cd01d0d90d013d0be7faefcef
Hernandes de Castro Guereta <132833897+Nandesfront@users.noreply.github.com>
2025-11-11 01:34:09 +0000

    configuracao postman

--- projeto-integrador/src/main/java/recursos/ProdutoRecurso.java
+++ projeto-integrador/src/main/java/recursos/ProdutoRecurso.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import entidades.Produto;
 import io.quarkus.panache.common.Sort;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -12,11 +14,15 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/produtos")
 public class ProdutoRecurso {
 
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Produto> listar() {
         return Produto.listAll(Sort.ascending("nome"));
     }
 
+    @POST
+    @Transactional
+    public void salvar(Produto produto) {
+        produto.persist();
+    }
 }
